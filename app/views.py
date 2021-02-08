32313f6fa42570cc444f582fa0711a633e8c3259
Edilson Jose Pateguana <64424952+EdilsonGH@users.noreply.github.com>
2021-02-08 20:41:14 +0200

    make site pro

--- app/views.py
+++ app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index1.html')
 def qrcode(request):
     return render(request, 'qrcode.html')
